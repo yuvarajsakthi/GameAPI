ed25793dc526d445bf95d4ebcac876360ee2e250
yuvaraj <yuvarajsakthi003@gmail.com>
2025-08-25 10:39:04 +0530

    Refactor BaseService methods in GameAPI.Services
    Removed and re-added methods in the BaseService class.
    No functional changes were made; this update may improve code organization or formatting.

--- GameAPI/Services/BaseService.cs
+++ GameAPI/Services/BaseService.cs
@@ -12,12 +12,12 @@ namespace GameAPI.Services
             _repository = repository;
         }
 
-        public virtual async Task<IEnumerable<T>> GetAllAsync() => await _repository.GetAllAsync();
-        public virtual async Task<T?> GetByIdAsync(int id) => await _repository.GetByIdAsync(id);
-        public virtual async Task<T> AddAsync(T entity) => await _repository.AddAsync(entity);
-        public virtual async Task<T> UpdateAsync(T entity) => await _repository.UpdateAsync(entity);
-        public virtual async Task<bool> DeleteAsync(int id) => await _repository.DeleteAsync(id);
-        public virtual async Task<IEnumerable<T>> SortAsync<TKey>(Func<T, TKey> keySelector, bool descending = false) => await _repository.SortAsync(keySelector, descending);
-        public virtual async Task<int> CountAsync(Func<T, bool>? predicate = null) => await _repository.CountAsync(predicate);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _repository.GetAllAsync();
+        public async Task<T?> GetByIdAsync(int id) => await _repository.GetByIdAsync(id);
+        public async Task<T> AddAsync(T entity) => await _repository.AddAsync(entity);
+        public async Task<T> UpdateAsync(T entity) => await _repository.UpdateAsync(entity);
+        public async Task<bool> DeleteAsync(int id) => await _repository.DeleteAsync(id);
+        public async Task<IEnumerable<T>> SortAsync<TKey>(Func<T, TKey> keySelector, bool descending = false) => await _repository.SortAsync(keySelector, descending);
+        public async Task<int> CountAsync(Func<T, bool>? predicate = null) => await _repository.CountAsync(predicate);
     }
 }
